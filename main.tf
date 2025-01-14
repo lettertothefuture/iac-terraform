cae0bec9a90f78a0813413d05dde39e76d05d42e
Gustavo Lizze <gustavolizze@outlook.com>
2025-01-14 12:17:19 -0300

    fix: terraform

--- main.tf
+++ main.tf
@@ -11,26 +11,3 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
-
-module "vpc" {
-  source = "./modules/vpc"
-}
-
-module "ecs" {
-  source    = "./modules/ecr"
-  subnet_id = module.vpc.ecs_subnet_id
-}
-
-module "ecr" {
-  source = "./modules/ecr"
-}
-
-module "s3" {
-  source                 = "./modules/s3"
-  react_site_bucket_name = var.react_site_bucket_name
-  letter_bucket_name     = var.letter_bucket_name
-}
-
-module "cloudfront" {
-  source = "./modules/cloudfront"
-}
